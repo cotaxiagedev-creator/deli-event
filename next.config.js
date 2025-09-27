d635971c10346482f49097bc6c5e6701801a085d
Chokri Soudane <cotaxiagedev@gmail.com>
2025-09-27 05:36:04 +0200

    chore: add next.config.js (outputFileTracingRoot)

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Ensures Next.js traces files relative to this project root when building
+  outputFileTracingRoot: __dirname,
+};
+
+module.exports = nextConfig;
